6497b13ec13b6697e9c2786a8267369442010f5b
Mike Goatly <mikegoatly@hotmail.com>
2023-04-26 19:38:52 +0100

    Run the Resona service under the user context

--- scripts/configure-auto-run.sh
+++ scripts/configure-auto-run.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-sudo sh -c "cat <<\EOT > /etc/systemd/system/
+sudo sh -c "cat <<\EOT > /lib/systemd/user/Resona.service
 [Unit]
 Description=Resona
-After=multi-user.target
+ConditionPathExists=/home/pi/bin
 
 [Service]
 #Redirecting output and error to log files is useful when debugging start up issues
-#StandardOutput=file:/home/pi/logs/logfile_stdout.log
-#StandardError=file:/home/pi/logs/logfile_stderr.log
-User=pi
+#StandardOutput=journal
+#StandardError=journal
 ExecStart=/home/pi/bin/Resona --drm
 WorkingDirectory=/home/pi/bin
 Restart=always
 RestartSec=0s
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=default.target
 EOT"
 
-sudo systemctl daemon-reload
+systemctl --user daemon-reload
 
-sudo systemctl enable Resona.service
+systemctl --user enable Resona.service
 
-sudo systemctl start Resona.service
+systemctl --user start Resona.service
--- scripts/reenable-auto-run.sh
+++ scripts/reenable-auto-run.sh
@@ -2,10 +2,10 @@
 
 SERVICE="Resona.service"
 
-if systemctl list-unit-files --type=service | grep -q "${SERVICE}"; then
+if systemctl --user list-unit-files --type=service | grep -q "${SERVICE}"; then
     echo "${SERVICE} is installed. Enabling and starting it now."
-    sudo systemctl enable "${SERVICE}"
-    sudo systemctl start "${SERVICE}"
+    systemctl --user enable "${SERVICE}"
+    systemctl --user start "${SERVICE}"
 else
     echo "${SERVICE} is not installed."
 fi
