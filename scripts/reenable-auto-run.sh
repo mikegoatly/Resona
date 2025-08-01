660ea030434faf6947b9df1024fa09a47eb2cabc
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2025-08-01 12:06:12 +0100

    Correct script

--- scripts/reenable-auto-run.sh
+++ scripts/reenable-auto-run.sh
@@ -4,8 +4,8 @@ SERVICE="Resona.service"
 
 if systemctl list-unit-files --user --type=service | grep -q "${SERVICE}"; then
     echo "${SERVICE} is installed. Enabling and starting it now."
-    sudo --user systemctl enable "${SERVICE}"
-    sudo --user systemctl start "${SERVICE}"
+    systemctl --user enable "${SERVICE}"
+    systemctl --user start "${SERVICE}"
 else
     echo "${SERVICE} is not installed."
 fi
